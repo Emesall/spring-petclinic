b5ee890ff1ba02597352c71432bc0f5376935549
Emesall <mati1230@gmail.com>
2022-01-16 18:00:52 +0100

    Initial commit

---
+++ src/test/java/com/emesall/petclinic/SpringUdemyPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emesall.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUdemyPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
