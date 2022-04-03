6360ef22cccbf6e05a31fbb58e9db0084e148a27
Emesall <mati1230@gmail.com>
2022-04-03 11:42:08 +0200

    Simple api for vets

--- petclinic-web/src/main/java/com/emesall/petclinic/controllers/VetController.java
+++ petclinic-web/src/main/java/com/emesall/petclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package com.emesall.petclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.emesall.petclinic.model.Vet;
 import com.emesall.petclinic.service.VetService;
 
 @Controller
@@ -24,4 +29,10 @@ public class VetController {
 		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
+	
+	@GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
+    }
 }
