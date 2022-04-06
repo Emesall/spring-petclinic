733cd482304869bee22456661eac16848b9c3f4b
Emesall <mati1230@gmail.com>
2022-04-06 05:21:44 +0200

    Login page added.

--- petclinic-web/src/main/java/com/emesall/petclinic/config/SecurityConfig.java
+++ petclinic-web/src/main/java/com/emesall/petclinic/config/SecurityConfig.java
@@ -1,15 +1,24 @@
 package com.emesall.petclinic.config;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -26,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
-		
+
 	}
 
 	@Override
@@ -39,12 +48,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(detailsService).passwordEncoder(encoder());
 
 	}
-	
+
 	@Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-            .ignoring()
-            .antMatchers("/h2-console/**");
-    }
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/h2-console/**");
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/owners/**")
+				.hasRole("OWNER")
+				.antMatchers("/", "/**")
+				.permitAll()
+				.and()
+				.formLogin()
+				.loginPage("/login");
+				
+
+	}
 
 }
